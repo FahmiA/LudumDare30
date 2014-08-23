5c2a7a9a22e6b37e044124ce9498476ecdd07925
Timothy Jones <git@zmthy.io>
2014-08-23 15:42:13 +1200

    Add simple panning script and removing meta ignore
    The panning causes the camera to move when the mouse gets too close to
    the edge of the screen.

---
+++ Assets/Scripts/Panning.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Panning : MonoBehaviour {
+
+    public int panThreshold = 20;
+    public int panAmount = 1;
+
+    void Update() {
+        Vector2 mousePosition = Input.mousePosition;
+
+        if (mousePosition.x < panThreshold) {
+            transform.Translate(new Vector2(-panAmount, 0));
+        } else if (mousePosition.x > Screen.width - panThreshold) {
+            transform.Translate(new Vector2(panAmount, 0));
+        }
+
+        if (mousePosition.y < panThreshold) {
+            transform.Translate(new Vector2(0, -panAmount));
+        } else if (mousePosition.y > Screen.height - panThreshold) {
+            transform.Translate(new Vector2(0, panAmount));
+        }
+
+        Debug.Log(transform.position);
+    }
+}
