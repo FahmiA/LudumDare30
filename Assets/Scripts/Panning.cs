a097ce57c3c89b8ea768892909139b8d8c8fd2b5
Timothy Jones <git@zmthy.io>
2014-08-23 16:45:21 +1200

    Adding more complicated panning script
    The panning now increases the close the mouse is to the edge of the
    screen. The arrow keys also cause a full pan.

--- Assets/Scripts/Panning.cs
+++ Assets/Scripts/Panning.cs
@@ -3,24 +3,53 @@ using System.Collections;
 
 public class Panning : MonoBehaviour {
 
+    // The distance of the mouse from the screen edge at which the camera should begin panning.
     public int panThreshold = 20;
-    public float panAmount = 0.2f;
+
+    // The maximum speed that pan should move at, in hundredths of a unit per frame.
+    public int maxPanSpeed = 20;
 
     void Update() {
         Vector2 mousePosition = Input.mousePosition;
 
+        float xdiff = 0;
+        float ydiff = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            xdiff -= panThreshold;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            xdiff += panThreshold;
+        }
+
         if (mousePosition.x < panThreshold) {
-            transform.Translate(new Vector2(-panAmount, 0));
+            xdiff -= Mathf.Min(panThreshold - mousePosition.x, panThreshold);
         } else if (mousePosition.x > Screen.width - panThreshold) {
-            transform.Translate(new Vector2(panAmount, 0));
+            xdiff += Mathf.Min(mousePosition.x - Screen.width + panThreshold, panThreshold);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            ydiff -= panThreshold;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            ydiff += panThreshold;
         }
 
         if (mousePosition.y < panThreshold) {
-            transform.Translate(new Vector2(0, -panAmount));
+            ydiff -= Mathf.Min(panThreshold - mousePosition.y, panThreshold);
         } else if (mousePosition.y > Screen.height - panThreshold) {
-            transform.Translate(new Vector2(0, panAmount));
+            ydiff += Mathf.Min(mousePosition.y - Screen.height + panThreshold, panThreshold);
         }
 
-        Debug.Log(transform.position);
+        if (xdiff != 0 || ydiff != 0) {
+            xdiff = Mathf.Clamp(xdiff, -panThreshold, panThreshold);
+            ydiff = Mathf.Clamp(ydiff, -panThreshold, panThreshold);
+
+            float mult = ((float) maxPanSpeed / 100) / panThreshold;
+
+            transform.Translate(new Vector2(xdiff * mult, ydiff * mult));
+        }
     }
 }
